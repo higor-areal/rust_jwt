a8869d531cc5bb14b6795e48e6b6c741a56e4875
Esau Higor <esauhigor@gmail.com>
2026-05-12 21:19:32 -0400

    função de health

---
+++ src/handlers/auth_handler.rs
@@ -0,0 +1,8 @@
+use axum::Json;
+use serde_json::{json, Value};
+
+pub async fn health() -> Json<Value> {
+    Json(json!({
+        "message": "API rodando"
+    }))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
+use axum::{
+    Router,
+    routing::get, serve::Listener
+};
+
+mod handlers;
+
+use handlers::auth_handler::{health};
+
+#[tokio::main]
+async fn main() {
+
+    let app = Router::new()
+    .route("/", get(health));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+
     println!("Hello, world!");
+    let _ = axum::serve(listener, app).await;
+ 
 }
