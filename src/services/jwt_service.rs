8dfa0d9724785933268ad4eb4323c8dcda2e05bf
Esau Higor <esauhigor@gmail.com>
2026-05-17 21:17:47 -0400

    acriando funções de jwt, ainda falta testes

--- src/services/jwt_service.rs
+++ src/services/jwt_service.rs
@@ -1,30 +1,55 @@
-use dotenvy::dotenv;
-use std::env;
-
 use serde::{Deserialize, Serialize};
+use jsonwebtoken::{
+    decode,
+    encode,
+    Algorithm,
+    DecodingKey,
+    EncodingKey,
+    Header,
+    Validation,
+};
+use chrono::{Utc, Duration};
 
-
-#[derive(Deserialize)]
-pub struct LoginResponse{
-    pub user_name: String,
-    pub password: String
-}
+use crate::models::user;
 
 #[derive(Deserialize, Serialize)]
 pub struct Claims{
     sub: String,
-    role: String,
     exp: usize
 }
 
-pub fn generate_token(){
+pub fn generate_token(user_name: String, secret: &str) -> String{
+    let expiration = Utc::now()
+        .checked_add_signed(Duration::hours(24))
+        .unwrap()
+        .timestamp() as usize;
+
+    let claims = Claims{
+        sub: user_name,
+        exp: expiration,
+    };
+
+    encode(
+        &Header::default(), 
+        &claims, 
+        &EncodingKey::from_secret(secret.as_bytes()))
+        .unwrap()
 
 }
 
-pub fn decode_token(){
+pub fn decode_token(token: &str, secret: &str) -> Claims{
+    let token_data = decode::<Claims>(   token, 
+        &DecodingKey::from_secret(secret.as_bytes()), 
+        &Validation::default())
+    .unwrap();
 
+        token_data.claims
 }
 
-pub fn verify_token(){
-
+pub fn verify_token(token: &str, secret: &str) -> bool{
+    decode::<Claims>(
+        token,
+        &DecodingKey::from_secret(secret.as_bytes()),
+        &Validation::new(Algorithm::HS256),
+    ).is_ok()
 }
